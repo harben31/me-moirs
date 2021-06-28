02edd4ec1c61e74766569b2276206cdd7de4820f
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-28 13:43:12 -0700

    created callback for seeding data to the Post model

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -65,3 +65,15 @@ db.Tab
         console.error(err);
         process.exit(1);
     });
+
+db.Post  
+    .remove({})
+    .then(() => db.Post.collection.insertMany(postSeed))
+    .then(data => {
+        console.log(data.result.n + 'records inserted!');
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
