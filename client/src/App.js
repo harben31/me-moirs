b9254588ab012175376576958eb410982102671b
Asia Alnahi <asia.alius@gmail.com>
2021-06-28 13:05:41 -0700

    Create the Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Home from './pages/Home';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 import "./App.css";
 
 function App() {
     return (
         <div className='App'>
+          <Header/>
           <Home/> 
+          <Footer/>
         </div>
     );
 };
---
+++ client/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <div className="left">
+                <div class="logo">
+                    <a href="#home">Application Name</a>
+                </div>
+            </div>
+        </div>
+    )
+}
