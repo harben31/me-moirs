0cd1ed44280a7a924c439be293f9e084db3948e4
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-28 22:18:22 -0700

    changed the ids from single digit strings to 24 character strings to fix error when making get request

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -52,9 +52,9 @@ export default function Cards() {
       
 return (
 
-  <div className= 'cards'>{cardInfo.map(card => {
+  <div className= 'cards'>{cardInfo.map((card, index) => {
      return (
-     <div className='cards'>
+     <div className='cards' key={index}>
        <div className= 'card-body'>
         <Card >
           <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://images.unsplash.com/photo-1483546363825-7ebf25fb7513?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3BlbiUyMGpvdXJuYWxzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60) center / cover'}}>{card.title}
