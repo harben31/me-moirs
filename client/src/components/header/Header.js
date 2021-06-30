b133471ece62ac1de9e2738583af48bfbb34e706
Asia Alnahi <asia.alius@gmail.com>
2021-06-29 21:10:22 -0700

    Change the wrapper class to header-wrapper in header cus we have the same class in the App.css and it is over writing the header style

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -5,7 +5,7 @@ import './style.css';
 export default function Header() {
     return (
         <div className='header'>
-            <div className='wrapper'>
+            <div className='header-wrapper'>
                 <div className='logo'> {/* changed from class to className  */} 
                     <a href='#home'>Name of the app.</a>
                     {/* <img src={image} height={100} width={100} /> */}
