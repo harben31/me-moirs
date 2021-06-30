b56b5ead9a11b6926be8959bf92310d327410ebd
Asia Alnahi <asia.alius@gmail.com>
2021-06-29 17:39:39 -0700

    Adding linked styled components to the form in the common.js file

--- client/src/components/AccountBox/common.js
+++ client/src/components/AccountBox/common.js
@@ -1,9 +1,31 @@
 import styled from 'styled-components';
 
 export const BoxContainer = styled.div`
-   width: 100%;
-   display: flex;
-   flex-direction: column;
-  align-items: center;
-  margin-top: 10px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10px;
+`;
+
+export const FormContainer = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
+`;
+
+export const MutedLink = styled.a`
+    font-size: 11px;
+    color: rgba(200, 200, 200, 0.8);
+    font-weight: 500;
+    text-decoration: none;
+`;
+
+export const BoldLink = styled.a`
+    font-size: 11px;
+    color: rgb(241, 196, 15);
+    font-weight: 500;
+    text-decoration: none;
+    margin: 0 4px;
 `;
