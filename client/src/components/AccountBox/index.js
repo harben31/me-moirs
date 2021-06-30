7445f292a8ffb8156cdfb8248ad5418399368bbb
Asia Alnahi <asia.alius@gmail.com>
2021-06-29 18:18:01 -0700

    Working on the index.js file that will contain the forms

---
+++ client/src/components/AccountBox/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const BoxContainer = styled.div`
+    width: 280px;
+    min-height: 550px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 19px;
+    background-color: #fff;
+    box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
+    position: relative;
+    overflow: hidden;
+`;
