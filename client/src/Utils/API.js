9ff5b70f3da0692b07bf510a8b9301626d56c689
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-07-03 11:26:32 -0700

    updating api function names

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -3,23 +3,23 @@ import axios from 'axios';
 
 export default {
 
-    // getUser: function() {
-    //     return axios.get('/api/users')
-    // },
+    getUser: function() {
+        return axios.get('/api/users')
+    },
 
 
-    // getUser: function(id) {
-    //     return axios.get('/api/users/me')
-    // },
+    getUsers: function(id) {
+        return axios.get('/api/users/me')
+    },
 
     getUser: function(id) {
         return axios.get('/api/users' + id)
     },
 
 
-    // deleteUser: function(id) {
-    //     return axios.delete('/api/users/' + id)
-    // },
+    deleteUser: function(id) {
+        return axios.delete('/api/users/' + id)
+    },
 
     saveUser: function(userData) {
         return axios.post('/api/users', userData)
@@ -27,38 +27,38 @@ export default {
 
 
 
-    getTab: function() {
+    getTabs: function() {
         return axios.get('/api/tabs')
     },
 
-    // getTab: function(id) {
-    //     return axios.get('/api/tabs' + id)
-    // },
+    getTab: function(id) {
+        return axios.get('/api/tabs' + id)
+    },
 
-    // deleteTab: function(id) {
-    //     return axios.delete('/api/tabs/' + id)
-    // },
+    deleteTab: function(id) {
+        return axios.delete('/api/tabs/' + id)
+    },
 
-    // saveTab: function(userData) {
-    //     return axios.put('/api/tabs', userData)
-    // },
+    saveTab: function(userData) {
+        return axios.put('/api/tabs', userData)
+    },
 
 
 
-    // getPost: function() {
-    //     return axios.get('/api/posts')
-    // },
+    getPosts: function() {
+        return axios.get('/api/posts')
+    },
 
-    // getPost: function(id) {
-    //     return axios.get('/api/posts' + id)
-    // },
+    getPost: function(id) {
+        return axios.get('/api/posts' + id)
+    },
 
-    // deletePost: function(id) {
-    //     return axios.delete('/api/posts/' + id)
-    // },
+    deletePost: function(id) {
+        return axios.delete('/api/posts/' + id)
+    },
 
-    // savePosts: function(userData) {
-    //     return axios.put('/api/posts', userData)
-    // },
+    savePost: function(userData) {
+        return axios.put('/api/posts', userData)
+    },
 
 }
