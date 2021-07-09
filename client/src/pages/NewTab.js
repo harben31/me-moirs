4f7acaa68e1bd0d90431ee76c0b4371631e96bf8
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-08 20:07:37 -0700

    added aside for tab description and a component for each post user creates

--- client/src/pages/NewTab.js
+++ client/src/pages/NewTab.js
@@ -1,13 +1,25 @@
 import React from 'react'
+// import PostForm from '../components/PostForm';
+import OldPost from '../components/OldPost';
 
 
 export default function NewTab() {
     return (
-        <div>
-            <div className= 'new-tabs'>
-                <h1>NEW TABS!!!!</h1>
+        <div className='tabBody'>
+            <aside className='description'>
+                <h5>About 'Tab Name'</h5>
+                <p>
+                    Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.
 
-            </div>
+                    Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to.
+
+                    Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow's nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.
+                </p>
+            </aside>
+            <section className='postSection'>
+                {/* <PostForm /> */}
+                <OldPost />
+            </section>
         </div>
     )
 }
