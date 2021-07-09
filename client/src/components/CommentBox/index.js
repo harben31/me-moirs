ba66663abd9206c8562d576ebe8a37268c3e14d9
Asia Alnahi <asia.alius@gmail.com>
2021-07-09 16:44:29 -0700

    Add Input component styled to the comment Box

--- client/src/components/CommentBox/index.js
+++ client/src/components/CommentBox/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import './style.css';
+
+
+const Input = styled.input`
+    width: 100%;
+    height: 42px;
+    outline: none;
+    margin-top: 10px;
+    border: 1px solid rgba(200, 200, 200, 0.3);
+    padding: 0px 10px;
+    border-radius: 9px;
+    border-bottom: 1.4px solid #0000001f;
+    transition: all 200ms ease-in-out;
+    font-size: 12px;
+
+    &::placeholder {
+        color: rgba(200, 200, 200, 1);
+    }
+
+    &:not(:last-of-type) {
+        border-bottom: 1.5px solid rgba(200, 200, 200, 0.4);
+    }
+    
+    &:focus {
+        outline: none;
+        border-bottom: 2px solid rgb(42, 157, 143);
+    }
+`;
+
 
 export default function CommentBox() {
     return (
@@ -7,7 +36,7 @@ export default function CommentBox() {
             <span class="material-icons">
                 chat
             </span> 
-            <input type='text' placeholder='Add a comment...'/>
+            <Input type='text' placeholder='Add a comment...'/>
             <span class="material-icons">
                 send
             </span>
