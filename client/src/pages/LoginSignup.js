8cd82413316b8cafa792a3a2e6af636cbe040e70
Asia Alnahi <asia.alius@gmail.com>
2021-07-14 12:35:09 -0700

    Add packground image to the login/signup page

--- client/src/pages/LoginSignup.js
+++ client/src/pages/LoginSignup.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {AccountBox} from '../components/AccountBox/index'
+import CoverPhoto from '../components/CoverPhoto/CoverPhoto';
 
 export default function LoginSignup() {
+    const [coverImage, setCoverImage] = useState([]);
+
+    useEffect(() => {
+        setCoverImage(
+           { 
+            cover_image:'https://images.unsplash.com/photo-1447069387593-a5de0862481e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b2xkJTIwam91cm5hbHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+           } 
+        )
+    }, [])
+
     return (
         <div>
+            <CoverPhoto image={coverImage.cover_image}/>
             <AccountBox/>
         </div>
     )
