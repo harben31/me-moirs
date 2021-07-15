66caa4b17607442777318c578ebf847715ed8080
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-07-14 19:38:42 -0700

    media query for image upload button

--- client/src/components/ProfileImage/ProfileImage.js
+++ client/src/components/ProfileImage/ProfileImage.js
@@ -63,8 +63,8 @@ export default function ProfileImage(props) {
             <div style={{position: 'relative'}}>
              <IconButton name="more_vert" id="demo-menu-lower-right" />
              <Menu target="demo-menu-lower-right" align="right">
-             <MenuItem><input type='file' name='file' onChange={uploadImage}/>Add A New Profile Image</MenuItem>
-             <MenuItem><input type='file' name='file' onChange={props.backgroundImage}/>Add A New Cover Photo</MenuItem>
+             <MenuItem className='profile-img'><input className='img-btn'type='file' name='file' onChange={uploadImage}/>Profile Image</MenuItem>
+             <MenuItem className='cover'><input className='img-btn'type='file' name='file' onChange={props.backgroundImage}/>Cover Photo</MenuItem>
             </Menu>
           </div>
          </div>
