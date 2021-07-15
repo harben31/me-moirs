d8ca1999e24b50584b69a18d3a07ea9e86ef76b9
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-07-14 21:09:46 -0700

    incoming merge

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { IconButton, MenuItem, Menu } from 'react-mdl';
 import './style.css';
 
 export default function Comment({formatDate, username, content, date}) {
     return (
         <div className='commentCard'>
-            <p className='commentDate'>
-                {formatDate(date)}
-            </p>
+            <div classame='commentTop'>
+                <p className='commentDate'>
+                    {formatDate(date)}
+                </p>
+                <span className='dotIcon'>
+                    <IconButton name="more_vert" id="demo-menu-lower-right" />
+                    <Menu target="demo-menu-lower-right" align="right">
+                        <MenuItem>Edit</MenuItem>
+                        <MenuItem>Delete</MenuItem>
+                    </Menu>
+                </span>
+            </div>
             <p className='commentTitle'>
                 <b>{username}</b>
             </p>
