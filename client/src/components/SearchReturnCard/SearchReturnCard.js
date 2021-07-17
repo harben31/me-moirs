52e49f426d2d22f03b938dbfb92eb18448b23a06
Asia Alnahi <asia.alius@gmail.com>
2021-07-17 14:36:32 -0700

    Add user image to the search return card

--- client/src/components/SearchReturnCard/SearchReturnCard.js
+++ client/src/components/SearchReturnCard/SearchReturnCard.js
@@ -30,18 +30,10 @@ export default function(props) {
         .catch(err => console.log(err))
     };
 
-    // useEffect(() => {
-    //     let newFriendsArray = props.friendsArray;
-    //     newFriendsArray.push(friend)
-    //     props.setFriendsArray(newFriendsArray);
-    //          console.log('New friend',newFriendsArray)
-
-    // }, [props.friendsArray])
-
     return (
          <div className='user-card'>
             <div className='user-info'>
-                <img className='user-image' src='https://images.pexels.com/photos/761963/pexels-photo-761963.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='photo'/>
+                <img className='user-image' src={props.image} alt={props.username}/>
 
                 <p className='user-name'>
                 {friend.username}
@@ -60,16 +52,5 @@ export default function(props) {
                 </button>
             </div>
         </div>
-        // <div className='userCard'>
-        //     <div className='infoWrap'>
-        //         <div className='imgStandIn'></div>
-        //         <p className='userEmail'>{friend.email}</p>
-        //     </div>
-        //     <h3 className='username'>{friend.username}</h3>
-        //     <button
-        //     className='addFriendBtn'
-        //     onClick={handleFollowReq}
-        //     >Follow</button>
-        // </div>
     );
 };
